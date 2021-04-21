74b32035a8234881f3e4d2fc1d2cc515f9dab0a0
vitaminwater <stantin04@gmail.com>
2021-04-21 11:03:30 +0200

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<!--
+      Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+      Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -->
+
+<template>
+  <section :id='$style.container'>
+    <Nuxt />
+  </section>
+</template>
+
+<style module lang=stylus>
+html
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif
+  font-size: 16px
+  word-spacing: 1px
+  -ms-text-size-adjust: 100%
+  -webkit-text-size-adjust: 100%
+  -moz-osx-font-smoothing: grayscale
+  -webkit-font-smoothing: antialiased
+  box-sizing: border-box
+  min-height: 100vh
+
+*,
+*::before,
+*::after
+  box-sizing: border-box
+  margin: 0
+
+#container
+  margin: 0 auto
+  min-height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+
+</style>
---
+++ store/auth.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+ * Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import Vue from 'vue'
+import axios from 'axios'
+
+import { loadFromStorage, saveToStorage } from '~/lib/client-side.js'
+
+const STORAGE_ITEM='supergreenlive'
+
+export const state = () => {
+  let defaults = {
+  };
+  return defaults
+};
+
+const storeState = (state) => {
+  saveToStorage(STORAGE_ITEM, JSON.stringify(state))
+}
+
+export const actions = {
+  nuxtClientInit(context) {
+    const saved = loadFromStorage(STORAGE_ITEM)
+    if (saved) {
+      context.commit('setState', JSON.parse(saved))
+    }
+  },
+}
+
+export const mutations = {
+  setState(state, newState) {
+    Object.assign(state, newState)
+  },
+}
+
+export const getters = {
+}
