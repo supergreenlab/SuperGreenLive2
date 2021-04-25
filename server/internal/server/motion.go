e4f59fdf8cfdf45c104b29927427bbea6b495eb2
vitaminwater <stantin04@gmail.com>
2021-04-25 11:37:40 +0200

    added start/stop motion endpoint

---
+++ server/internal/server/motion.go
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+ * Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+)
+
+var cmd *exec.Cmd
+
+func motionHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if cmd != nil {
+		if err := cmd.Process.Kill(); err != nil {
+			log.Fatal("failed to kill process: ", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		fmt.Fprintf(w, "0")
+		cmd = nil
+		return
+	}
+	cmd = exec.Command("/usr/bin/motion")
+	cmd.Stdout = os.Stdout
+	if err := cmd.Start(); err != nil {
+		logrus.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	logrus.Info("Motion started")
+	fmt.Fprintf(w, "1")
+}
