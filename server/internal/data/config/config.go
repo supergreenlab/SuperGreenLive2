6447bbf17f0e14708fc8d41af425411c2f7ec4fa
vitaminwater <stantin04@gmail.com>
2021-04-25 13:07:35 +0200

    fixed gitignore ignoring internal/data/config

---
+++ server/internal/data/config/config.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+ * Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+func Init() {
+	viper.SetConfigName("liveserver")
+	viper.AddConfigPath("/etc/liveserver")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
+
+	viper.SetEnvPrefix("LIVESERVER")
+	viper.AutomaticEnv()
+
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+}
