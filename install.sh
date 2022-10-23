51cd8d95f3a3eee56d51ee61d1ba5dd7ea03cddc
black-161-flag <github@blackbirds.xyz>
2022-10-23 20:38:38 +0200

    Update libcamera-streamer version

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get --allow-releaseinfo-change update
 sudo apt-get install -y fswebcam ffmpeg motion libmagickwand-dev libatlas-base-dev libopenjp2-7 \
                         python3-pip python3-libcamera python3-kms++ python3-prctl
 
-pip3 install https://github.com/black-161-flag/libcamera-streamer/releases/download/0.0.1/libcamera-streamer-0.0.1.tar.gz
+pip3 install https://github.com/black-161-flag/libcamera-streamer/releases/download/0.0.2/libcamera-streamer-0.0.2.tar.gz
 
 curl -OL https://github.com/supergreenlab/SuperGreenLive2/releases/download/latest/liveserver.zip
 unzip -o liveserver.zip
