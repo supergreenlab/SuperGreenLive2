523dce3d745d2e5fc913c4292d60b3720e0c5578
black-161-flag <github@blackbirds.xyz>
2022-10-27 20:45:49 +0200

    Refactore install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,40 @@
 
 set -e
 
-if [ "$(/usr/bin/lsb_release -rs)" -le "10" ]; then
-  echo "running on debian buster or older"
-  apt-get --allow-releaseinfo-change update
-else
-  apt-get update
-fi
+apt-get --allow-releaseinfo-change update
 
-apt-get install -y fswebcam ffmpeg libmagickwand-dev libatlas-base-dev libopenjp2-7 \
-                        python3-pip python3-prctl libgtk-3-0
+apt-get install -y fswebcam ffmpeg libmagickwand-dev \
+                   python3-opencv python3-picamera
 
 if [ "$(/usr/bin/lsb_release -rs)" -ge "11" ]; then
-  echo "running on debian bullseye or greater"
-  apt-get install -y python3-libcamera python3-kms++ libcamera-apps-lite
-  pip3 install --upgrade numpy
-  pip3 install https://github.com/black-161-flag/libcamera-streamer/releases/download/0.0.4/libcamera-streamer.tar.gz
+  echo "running on debian bullseye or newer"
+  apt-get install -y python3-libcamera python3-picamera2
+  curl --location \
+       --output /usr/local/bin/libcamera-streamer \
+       https://raw.githubusercontent.com/black-161-flag/libcamera-streamer/main/bin/libcamera-streamer
+  chmod +x /usr/local/bin/libcamera-streamer
   apt --reinstall install -y libcamera-apps-lite
+else
+  echo "running on debian buster or older"
+  apt-get install -y python3-pip libatlas-base-dev
+  pip3 install simplejpeg
+  pip3 install --upgrade numpy -i https://www.piwheels.org/simple
 fi
-pip3 install https://github.com/black-161-flag/usbcam-streamer/releases/download/0.0.3/usbcam-streamer.tar.gz
-pip3 install https://github.com/black-161-flag/picamera-streamer/releases/download/0.0.2/picamera-streamer.tar.gz
+
+curl --location \
+     --output /usr/local/bin/picamera-streamer \
+     https://raw.githubusercontent.com/black-161-flag/picamera-streamer/main/bin/picamera-streamer
+chmod +x /usr/local/bin/picamera-streamer
+
+curl --location \
+     --output /usr/local/bin/usbcam-streamer \
+     https://raw.githubusercontent.com/black-161-flag/usbcam-streamer/main/bin/usbcam-streamer
+chmod +x /usr/local/bin/usbcam-streamer
 
 # curl -OL https://github.com/supergreenlab/SuperGreenLive2/releases/download/latest/liveserver.zip
-curl -OL https://github.com/black-161-flag/SuperGreenLive2/releases/download/v0.0.3beta/liveserver.zip
+curl --remote-name \
+     --location \
+     https://github.com/black-161-flag/SuperGreenLive2/releases/download/v0.0.3beta/liveserver.zip
 unzip -o liveserver.zip
 
 mkdir -p /usr/local/share/appbackend /usr/local/share/appbackend_static
