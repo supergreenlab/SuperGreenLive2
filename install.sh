32a8dfb0de6257d1f6f03da5521a546659be29c7
black-161-flag <github@blackbirds.xyz>
2022-10-28 10:26:25 +0200

    Working installer for buster and bullseye

--- install.sh
+++ install.sh
@@ -4,22 +4,27 @@ set -e
 
 apt-get --allow-releaseinfo-change update
 
-apt-get install -y fswebcam ffmpeg libmagickwand-dev \
-                   python3-opencv python3-picamera
+apt-get install --yes \
+        fswebcam ffmpeg libmagickwand-dev \
+        python3-opencv python3-picamera
 
 if [ "$(/usr/bin/lsb_release -rs)" -ge "11" ]; then
   echo "running on debian bullseye or newer"
-  apt-get install -y python3-libcamera python3-picamera2
+  apt-get install --yes \
+          python3-libcamera python3-picamera2
   curl --location \
        --output /usr/local/bin/libcamera-streamer \
        https://raw.githubusercontent.com/black-161-flag/libcamera-streamer/main/bin/libcamera-streamer
   chmod +x /usr/local/bin/libcamera-streamer
-  apt --reinstall install -y libcamera-apps-lite
+  apt --reinstall install --yes libcamera-apps-lite
 else
   echo "running on debian buster or older"
-  apt-get install -y python3-pip libatlas-base-dev
+  apt-get install --yes \
+          python3-pip libatlas-base-dev
   pip3 install simplejpeg
-  pip3 install --upgrade numpy -i https://www.piwheels.org/simple
+  pip3 install numpy \
+       --upgrade \
+       --index-url https://www.piwheels.org/simple
 fi
 
 curl --location \
@@ -38,10 +43,10 @@ curl --remote-name \
      https://github.com/black-161-flag/SuperGreenLive2/releases/download/v0.0.3beta/liveserver.zip
 unzip -o liveserver.zip
 
-mkdir -p /usr/local/share/appbackend /usr/local/share/appbackend_static
+mkdir --parents /usr/local/share/appbackend /usr/local/share/appbackend_static
 
-cp -r liveserver/assets/* /usr/local/share/appbackend
-cp -r liveserver/static/* /usr/local/share/appbackend_static
+cp --recursive liveserver/assets/* /usr/local/share/appbackend
+cp --recursive liveserver/static/* /usr/local/share/appbackend_static
 cp liveserver/liveserver /usr/local/bin/liveserver
 
 #if [ "$(dpkg --print-architecture)" = "arm64" ]; then
@@ -50,7 +55,7 @@ cp liveserver/liveserver /usr/local/bin/liveserver
 #  cp liveserver/liveserver_arm32 /usr/local/bin/liveserver
 #fi
 
-mkdir -p /etc/liveserver
+mkdir --parents /etc/liveserver
 cp liveserver/liveserver.toml /etc/liveserver/liveserver.toml
 
 cp liveserver/liveserver.service /etc/systemd/system/
