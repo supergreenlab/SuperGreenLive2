dec521cf42032f0d9b5180e2c3fceb7387464ce6
vitaminwater <stantin04@gmail.com>
2024-09-06 16:45:34 +0200

    added start_dev.sh

---
+++ start_dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# RPI_URL=http://sglive.local:8081 API_URL=http://192.168.1.49:8080 HOST=0.0.0.0 yarn dev
+RPI_URL=http://192.168.1.49:8081 API_URL=http://192.168.1.49:8090 HOST=0.0.0.0 yarn dev
